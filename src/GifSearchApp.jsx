93c465d33d6c731d566342ca9f11a3c1df7e9f88
mbernallaura <mbernallaura@gmail.com>
2023-02-13 18:03:05 -0500

    Darle estilos a la pagina, agregar elementos a una lista ordenada,

--- src/GifSearchApp.jsx
+++ src/GifSearchApp.jsx
@@ -1,9 +1,28 @@
-
+import { useState } from "react";
 
 export const GifSearchApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon ball']);
+ 
+  const onAddCategory = () =>{
+    setCategories(['valorant', ...categories ]);
+  }
+
   return (
     <>
-        <h1>GifSearchApp</h1>
+      {/* Titulo */}
+      <h1>GifSearchApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gifs */}
+      <button onClick={ onAddCategory }>Agregar</button>
+      <ol>
+        {categories.map( category => {
+            return <li key={ category }>{category}</li>
+          })
+        }
+      </ol>
+        {/* Gif item */}
     </>
   )
 }
