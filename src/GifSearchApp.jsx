ceb895b7a5588ead0aa7a9a667e6ba6f8f6eda58
mbernallaura <mbernallaura@gmail.com>
2023-02-13 20:10:18 -0500

    Compaginar componentes

--- src/GifSearchApp.jsx
+++ src/GifSearchApp.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifSearchApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon ball']);
  
   const onAddCategory = () =>{
     setCategories(['valorant', ...categories ]);
+    // setCategories(cat => [...cat, 'valorant'])
   }
 
   return (
@@ -13,6 +15,7 @@ export const GifSearchApp = () => {
       <h1>GifSearchApp</h1>
 
       {/* Input */}
+      <AddCategory/>
 
       {/* Listado de Gifs */}
       <button onClick={ onAddCategory }>Agregar</button>
