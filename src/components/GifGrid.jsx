9e2d27848174e4a3372393c44e35a3e3dc6c291b
mbernallaura <mbernallaura@gmail.com>
2023-02-15 19:50:43 -0500

    Utilizar el hook useEffect para que solo una vez haga el fetch de los gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,16 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+
+    useEffect(() =>{
+        getGifs(category);
+    },[])
     
-    getGifs(category);
 
     return (
         <>
             <h3>{ category }</h3>
-            <p>Hola mundo</p>
         </>
     )
 }
